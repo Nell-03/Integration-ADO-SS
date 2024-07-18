5b7f1b0b81384cdaa719e1762546a3111b2746a9
Nell-03 <130708055+Nell-03@users.noreply.github.com>
2024-07-18 09:15:19 -0400

    Update Config.py

--- ADOIntegration/Config.py
+++ ADOIntegration/Config.py
@@ -1,12 +1,12 @@
 #Config.py
 # Azure DevOps constants
-azure_org_url = "https://dev.azure.com/lwhite0579"
-azure_personal_token = "caquomngcjqq3tlpohcjrc2byfkrwdoqc4ujip7ixaldtx6z7plq"
-azure_project_name = "Demo"
+azure_org_url = " "
+azure_personal_token = " "
+azure_project_name = " "
 
 
-# Replace 'YOUR_API_KEY' with your Smartsheet API Key
-api_key = 'c2crtwuzER4YLTxJtsN6JmApadSr49PmhCk1k'
+# Replace 'YOUR_API_KEY' with your Smartsheet Personal Access Key
+api_key = ' '
 # Replace 'YOUR_SHEET_ID' with the ID of the Smartsheet you want to fetch data from
-sheet_id = '3097333692583812'
-projectlist_sheetid = '7200971872489348'
+sheet_id = ' '
+projectlist_sheetid = ' '
